928ac478a8308e4c2693a8f8cb1b1e84c5e04596
Carlos <carlos@falconmasters.com>
2021-11-11 13:27:13 -0600

    Agregando scroll infinito

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 let pagina = 1;
-const btnAnterior = document.getElementById('btnAnterior');
-const btnSiguiente = document.getElementById('btnSiguiente');
+let peliculas = '';
+let ultimaPelicula;
 
-btnSiguiente.addEventListener('click', () => {
-	if(pagina < 1000){
-		pagina += 1;
-		cargarPeliculas();
-	}
+// Creamos el observador
+let observador = new IntersectionObserver((entradas, observador) => {
+	console.log(entradas);
+
+	entradas.forEach(entrada => {
+		if(entrada.isIntersecting){
+			pagina++;
+			cargarPeliculas();
+		}
+	});
+}, {
+	rootMargin: '0px 0px 200px 0px',
+	threshold: 1.0
 });
 
-btnAnterior.addEventListener('click', () => {
-	if(pagina > 1){
-		pagina -= 1;
-		cargarPeliculas();
-	}
-});
 
 const cargarPeliculas = async() => {
 	try {
 		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
 	
-		console.log(respuesta);
+		// console.log(respuesta);
 
 		// Si la respuesta es correcta
 		if(respuesta.status === 200){
 			const datos = await respuesta.json();
 			
-			let peliculas = '';
 			datos.results.forEach(pelicula => {
 				peliculas += `
 					<div class="pelicula">
@@ -37,6 +38,16 @@ const cargarPeliculas = async() => {
 			});
 
 			document.getElementById('contenedor').innerHTML = peliculas;
+
+			if(pagina < 1000){
+				if(ultimaPelicula){
+					observador.unobserve(ultimaPelicula);
+				}
+	
+				const peliculasEnPantalla = document.querySelectorAll('.contenedor .pelicula');
+				ultimaPelicula = peliculasEnPantalla[peliculasEnPantalla.length - 1];
+				observador.observe(ultimaPelicula);
+			}
 
 		} else if(respuesta.status === 401){
 			console.log('Pusiste la llave mal');
